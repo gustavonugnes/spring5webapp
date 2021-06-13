68873fba78040eb4c8b961edf5d1f5e242c8e7cc
Gustavo Nugnes <gustavo.nugnes@gmail.com>
2021-06-13 01:52:54 -0300

    Added first entities & repositories

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,48 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "234243");
+        Publisher p1 = new Publisher("123", "Boca Chica", "Texas", "456");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of books: "+bookRepository.count());
+        System.out.println("New publisher: "+p1.toString());
+    }
+}
